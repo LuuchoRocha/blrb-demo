1ea6bd7c08f39759b0332e47be45abad2f3b757b
Luciano Rocha <lucho@plum.com.ar>
2020-11-11 03:50:36 -0300

    Add glow effect based on audio input.

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ import './index.css';
 function lerp(start, end, t) {
   return start * (1 - t) + end * t;
 }
+
 const BLRB = () => {
   const [recording, setRecording] = useState(false);
   const [chunks, setChunks] = useState([]);
   const [src, setSrc] = useState('');
   const [width, setWidth] = useState(window.innerWidth);
   const [height, setHeight] = useState(window.innerHeight);
+  const [border, setBorder] = useState(0);
 
   const stream = useRef(null);
   const mediaRecorder = useRef(null);
@@ -23,40 +25,58 @@ const BLRB = () => {
   const analyser = useRef(null);
 
   const animate = useCallback(() => {
-    if (analyser.current) {
-      let bufferLength = analyser.current.frequencyBinCount;
-      let dataArray = new Uint8Array(bufferLength);
+    let bufferLength = analyser.current.frequencyBinCount;
+    let dataArray = new Uint8Array(bufferLength);
+    let dataTimeArray = new Uint8Array(bufferLength);
+
+    analyser.current.getByteFrequencyData(dataArray);
+    analyser.current.getByteTimeDomainData(dataTimeArray);
+
+    let osc = document.getElementById('oscilloscope');
+    let oscCtx = osc.getContext('2d');
+
+    // let metter = document.getElementById('metter');
+    // let metterContext = metter.getContext('2d');
+
+    oscCtx.clearRect(0, 0, width, height);
+
+    const draw = function () {
+      frame.current = requestAnimationFrame(draw);
 
       analyser.current.getByteFrequencyData(dataArray);
 
-      let canvas = document.getElementById('oscilloscope');
-      let canvasCtx = canvas.getContext('2d');
+      oscCtx.fillStyle = '#1f1f1f';
+      oscCtx.fillRect(0, 0, width, height);
 
-      canvasCtx.clearRect(0, 0, width, height);
+      let barWidth = width / bufferLength;
+      let barHeight;
+      let x = 0;
+      let values = 0;
 
-      let drawAlt = function () {
-        frame.current = requestAnimationFrame(drawAlt);
+      for (let i = 0; i < bufferLength; i++) {
+        values += dataArray[i];
 
-        analyser.current.getByteFrequencyData(dataArray);
-        canvasCtx.fillStyle = '#1f1f1f';
-        canvasCtx.fillRect(0, 0, width, height);
+        barHeight = lerp(0, height, dataArray[i] / 255) - 1;
 
-        let barWidth = width / bufferLength;
-        let barHeight;
-        let x = 0;
+        oscCtx.fillStyle = '#9977ff';
+        oscCtx.fillRect(x, height - barHeight, barWidth, barHeight);
 
-        for (let i = 0; i < bufferLength; i++) {
-          barHeight = lerp(0, height, dataArray[i] / 255);
+        x += barWidth + 3;
+      }
 
-          canvasCtx.fillStyle = '#9977ff';
-          canvasCtx.fillRect(x, height - barHeight, barWidth, barHeight);
+      // metterContext.clearRect(0, 0, width, height);
+      // metterContext.fillStyle = 'rgba(96, 96, 96, 0.2)';
+      // metterContext.fillRect(
+      //   0,
+      //   0,
+      //   width,
+      //   lerp(0, height, values / bufferLength / 255)
+      // );
 
-          x += barWidth + 10;
-        }
-      };
+      setBorder(Math.round(values / bufferLength));
+    };
 
-      drawAlt();
-    }
+    draw();
   }, [height, width]);
 
   const handleDataAvailable = useCallback(
@@ -99,10 +119,22 @@ const BLRB = () => {
         mediaStream.current = audioCtx.current.createMediaStreamSource(s);
         analyser.current = audioCtx.current.createAnalyser();
         analyser.current.fftSize = 128;
+        analyser.current.smoothingTimeConstant = 0.9;
         mediaStream.current.connect(analyser.current);
         animate();
       });
+
+      window.onresize = () => {
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
+      };
     }
+
+    return () => {
+      if (frame.current) {
+        cancelAnimationFrame(frame.current);
+      }
+    };
   }, [animate]);
 
   return (
@@ -113,7 +145,12 @@ const BLRB = () => {
       >
         <canvas id="oscilloscope" width={width} height={height} />
       </div>
-      <div className="recordButton" onClick={handleOnClick}>
+      <canvas id="metter" width={width} height={height} />
+      <div
+        className="recordButton"
+        onClick={handleOnClick}
+        style={{boxShadow: `0px 0px ${border}px ${border/2}px #9977ffcc`}}
+      >
         <MicRounded className={'mic ' + (recording ? 'recording' : '')} />
       </div>
       <audio src={src} controls={true}></audio>
