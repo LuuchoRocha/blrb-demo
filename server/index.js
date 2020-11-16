dc971f4af987b05405c21f978c43583e969152f2
Luciano Rocha <lucho@plum.com.ar>
2020-11-16 06:40:53 -0300

    Refactor server.

--- server/index.js
+++ server/index.js
@@ -1,35 +1,65 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger');
-const app = express();
 const http = require('http');
 const io = require('socket.io');
 const ss = require('socket.io-stream');
-const path = require('path');
 const fs = require('fs');
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(pino());
+class Server {
+  constructor() {
+    this.clients = {};
+    this.app = express();
+    this.addMiddlewares();
+    this.defineRoutes();
+    this.createServer();
+    this.createSocket();
+  }
 
-app.get('/api/greeting', (req, res) => {
-  const name = req.query.name || 'World';
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({greeting: `Hello ${name}!`}));
-});
+  addMiddlewares() {
+    this.app.use(bodyParser.urlencoded({extended: false}));
+    this.app.use(pino());
+  }
 
-const server = http.createServer(app);
+  createServer() {
+    this.server = http.createServer(this.app);
+  }
 
-const socket = io(server);
+  createSocket() {
+    this.socket = io(this.server);
 
-socket.on('connection', async (socket) => {
-  const remoteAddress = socket.handshake.headers['x-forwarded-for'];
-  console.log('Connected from ' + remoteAddress);
-  socket.emit('success', remoteAddress);
-  ss(socket).on('stream', function (stream, data) {
-    stream.pipe(fs.createWriteStream(path.basename('stream.wav'), {flags: 'a'}));
-  });
-});
+    this.socket.on('connection', (socket) => {
+      const remoteAddress = socket.handshake.headers['x-forwarded-for'];
+      console.log('Connected from ' + remoteAddress);
 
-server.listen(3001, () => {
-  console.log('Server up and listening...');
-});
+      socket.emit('success', remoteAddress);
+
+      socket.on('start', () => {
+        this.clients[remoteAddress] = {
+          id: remoteAddress,
+          file: `streams/${remoteAddress.replace(/[^\w]/gi, '-')}-${Date.now()}.wav`,
+        }
+      });
+
+      ss(socket).on('stream', (stream, _data) => {
+        stream.pipe(fs.createWriteStream(this.clients[remoteAddress].file, {flags: 'a'}));
+      });
+    });
+  }
+
+  defineRoutes() {
+    this.app.get('/api/greeting', (req, res) => {
+      const name = req.query.name || 'World';
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify({greeting: `Hello ${name}!`}));
+    });
+  }
+
+  listen() {
+    this.server.listen(3001, () => {
+      console.log('Server up and listening...');
+    });
+  }
+}
+
+new Server().listen();
